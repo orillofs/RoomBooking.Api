3c4691dd9b90fc641fc55c23107d9a01ba4d9eb8
orillofs <borillo@fullscale.ph>
2026-08-18 11:37:56 +0800

    Feature: Add xmin concurrency token and GiST exclusion constraint for overlapping bookings

--- Models/DTOs/BookingResponse.cs
+++ Models/DTOs/BookingResponse.cs
@@ -9,4 +9,5 @@ public class BookingResponse
     public int RoomId { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
+    public uint Version { get; set; }
 }
--- Models/Entities/Booking.cs
+++ Models/Entities/Booking.cs
@@ -9,6 +9,7 @@ public class Booking
     public int RoomId { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
+    public uint Version { get; private set; }
     public Room Room { get; set; } = null!;
     public User User { get; set; } = null!;
 }
